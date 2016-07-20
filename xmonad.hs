cf8faf867f83c433b2926ed49a10164f451fe994
Thomas Schneider <qsuscs@qsuscs.de>
2016-07-20 22:51:17 +0200

    Add key shortcut to lock screen
    Signed-off-by: Thomas Schneider <qsuscs@qsuscs.de>

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,11 @@
 import           XMonad
+import           XMonad.Util.EZConfig
 
 main = do
   xmonad $ defaultConfig
     { terminal = "gnome-terminal"
     , modMask = mod4Mask
     }
+    `additionalKeysP`
+    [ ("M-s", spawn "s")
+    ]
