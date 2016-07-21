4965018d854a94a42377a3aa352da78344a2b038
Thomas Schneider <qsuscs@qsuscs.de>
2016-07-21 13:18:37 +0200

    Change i3lock color
    Signed-off-by: Thomas Schneider <qsuscs@qsuscs.de>

--- xmonad.hs
+++ xmonad.hs
@@ -7,6 +7,6 @@ main = do
     , modMask = mod4Mask
     }
     `additionalKeysP`
-    [ ("M-s", spawn "i3lock -c 220022")
-    , ("M-S-s", spawn "i3lock -c 220022 && systemctl hybrid-sleep")
+    [ ("M-s", spawn "i3lock -c aa00aa")
+    , ("M-S-s", spawn "i3lock -c aa00aa && systemctl hybrid-sleep")
     ]
