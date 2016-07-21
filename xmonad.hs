898ec6c6338df701c7d3db3be716ee8076503502
Thomas Schneider <qsuscs@qsuscs.de>
2016-07-21 17:28:22 +0200

    Add helper function for helper script
    Signed-off-by: Thomas Schneider <qsuscs@qsuscs.de>

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,16 @@
 import           XMonad
 import           XMonad.Util.EZConfig
 
+helper x = spawn $ "~/.xmonad/helper.sh " ++ x
+
 main = do
   xmonad $ defaultConfig
     { terminal = "gnome-terminal"
     , modMask = mod4Mask
     }
     `additionalKeysP`
-    [ ("M-s", spawn "~/.xmonad/helper.sh lock")
-    , ("M-S-s", spawn "~/.xmonad/helper.sh lock-hybrid-sleep")
+    [ ("M-s", helper "lock")
+    , ("M-S-s", helper "lock-hybrid-sleep")
     , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
     , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
     ]
